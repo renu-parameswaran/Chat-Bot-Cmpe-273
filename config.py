1721aa1cdeb0fbda9a33e85c2d2ea151de2ad0cb
Y.Arun Ramanathan <y.arunram@gmail.com>
2017-04-26 19:38:42 -0700

    Deleting the config.
    Pushed by mistake.

--- config.py
+++
@@ -1,8 +0,0 @@
-appKey = "xoxb-159087266007-OOZLYQ5ZSiGy0Brys7HjxenE"
-
-dbHost="localhost"
-dbUser="root"
-dbPassword=""
-dbName="sarasjsu"
-
-questionList = ['who','where','what','why','when','how']
