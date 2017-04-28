9b58cda3cf2883f40aa0a87eda11a372eae85582
renu-parameswaran <renu.parameswaran@gmail.com>
2017-04-27 17:25:02 -0700

    Adding new function
    New function to handle default replies.

--- botController.py
+++ botController.py
@@ -5,14 +5,39 @@ def getReply(userInput):
     userInputArray = lang_processor.split_message(userInput)
     userInputWithOnlyQuestionAndKeywords = lang_processor.removeUnwantedWords(userInputArray)
     questions,keywords = lang_processor.seperateQuestionAndKeywords(userInputWithOnlyQuestionAndKeywords)
-    #questions,keywords = lang_processor.seperateQuestionAndKeywords(userInputArray)
+	
     conn = database.connectToDB()
     if(conn!="error"):
         database.getAllResponses(questions[0],conn)
-    
-    response = "This is sara. How can I help?"
-    if userInput.startswith("do"):
-        response = "Need some intelligence first"
+   
+	
+    response = handle_request(questions,keywords)
+    return response
+
+
+def handle_request(questions,Keywords):
+    count = len(questions)
+    if (count == 0):
+     response =  common_replies(Keywords)
+    elif(count == 1):
+       response = "hello this is from renu's code"
+    else :
+     response = "please input proper question format to handle them"
 
     return response
+
+
+
+def common_replies(user_input):
+
+ for i in user_input:
+    if i in ("Hi sara"):
+      response = "Hi! Welcome"
+    elif i in ("Thanks Sara"):
+     response = "Welcome Sara"
+    elif i in ("Its a great day"):
+     response = "Yes, A Wonderful Day."
+    else:
+     response = "Sorry! Cannot Handle"
+ return response
 
