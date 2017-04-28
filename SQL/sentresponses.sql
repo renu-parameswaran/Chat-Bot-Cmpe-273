40778cf6499d78fb42964019c25f3bf3b3daaab6
renu-parameswaran <renu.parameswaran@gmail.com>
2017-04-27 17:11:44 -0700

    add table
    Added sentresponses table

---
+++ SQL/sentresponses.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2017 at 01:54 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sarasjsu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sentresponses`
+--
+
+CREATE TABLE `sentresponses` (
+  `ID` int(100) NOT NULL,
+  `UserQuestion` varchar(300) NOT NULL,
+  `Answer` varchar(300) NOT NULL,
+  `MatchingKeywords` varchar(300) NOT NULL,
+  `QuestionPart` varchar(300) NOT NULL,
+  `CurrentScore` int(100) NOT NULL,
+  `Timestamp` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
