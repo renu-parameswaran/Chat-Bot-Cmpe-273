1c2e617182183f0138e315b82061997499bc65e4
Y.Arun Ramanathan <y.arunram@gmail.com>
2017-04-28 19:50:58 -0700

    Adding AI to ID field

--- SQL/sentresponses.sql
+++ SQL/sentresponses.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2017 at 01:54 AM
+-- Generation Time: Apr 29, 2017 at 04:49 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -28,14 +28,50 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `sentresponses` (
   `ID` int(100) NOT NULL,
-  `UserQuestion` varchar(300) NOT NULL,
-  `Answer` varchar(300) NOT NULL,
-  `MatchingKeywords` varchar(300) NOT NULL,
-  `QuestionPart` varchar(300) NOT NULL,
+  `UserQuestion` varchar(2048) NOT NULL,
+  `Answer` varchar(2048) NOT NULL,
+  `MatchingKeywords` varchar(2048) NOT NULL,
+  `QuestionPart` varchar(2048) NOT NULL,
   `CurrentScore` int(100) NOT NULL,
-  `Timestamp` varchar(500) NOT NULL
+  `Timestamp` varchar(2048) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `sentresponses`
+--
+
+INSERT INTO `sentresponses` (`ID`, `UserQuestion`, `Answer`, `MatchingKeywords`, `QuestionPart`, `CurrentScore`, `Timestamp`) VALUES
+(1, 'where is the classroom for 273', 'Hello I am sara', 'is,classroom,273 ', 'where', 1, '2017-04-28 18:26:17.425000'),
+(2, 'who is the instructor for 273', 'Hello I am sara', 'is,instructor,273 ', 'who', 1, '2017-04-28 18:27:00.157000'),
+(3, 'who is the instructor for 273', 'Hello I am sara', 'is,instructor,273 ', 'who', 1, '2017-04-28 18:29:17.381000'),
+(4, 'who is the instructor for 273', 'Hello I am sara', 'is,instructor,273 ', 'who', 1, '2017-04-28 18:30:17.969000'),
+(5, 'who is the instructor for 273', 'Hello I am sara', 'is,instructor,273 ', 'who', 1, '2017-04-28 18:30:25.221000'),
+(6, 'who is the instructor for 273', 'Hello I am sara', 'is,instructor,273 ', 'who', 1, '2017-04-28 18:30:31.392000'),
+(7, 'who is the instructor for 273', 'Hello I am sara', 'is,instructor,273 ', 'who', 1, '2017-04-28 18:30:37.608000'),
+(8, 'who is the instructor for 273', 'Hello I am sara', 'is,instructor,273 ', 'who', 1, '2017-04-28 18:57:33.506000'),
+(9, 'where is 273', 'Hello I am sara', 'is,273 ', 'where', 1, '2017-04-28 18:57:50.308000'),
+(10, 'where is 273', 'Hello this is sara', 'is,273 ', 'where', 1, '2017-04-28 18:58:26.953000'),
+(11, 'who is 273', 'Hello this is sara', 'is,273 ', 'who', 1, '2017-04-28 19:19:00.967000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sentresponses`
+--
+ALTER TABLE `sentresponses`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sentresponses`
+--
+ALTER TABLE `sentresponses`
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
