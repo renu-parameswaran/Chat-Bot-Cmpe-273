2927e5a8fd3eacd8d34955a06aa198a92f6791cc
Smitha Venkatesh <Smithavenkatesh@users.noreply.github.com>
2017-05-09 16:04:01 -0700

    Adding function to log for every user input questions and answers

--- log.py
+++ log.py
@@ -1,3 +1,7 @@
+import shutil
+import os
+import config
+
 fileName = "file1.txt"
 
 def truncateFile():
@@ -7,4 +11,14 @@ def writetofile(content):
 
    with open(fileName, "a") as text_file:
         text_file.write("\n" + content)
+
+
+def writetologfile(fileLog):
+    fullLogPath = config.logpath + fileLog
+    if not os.path.exists(os.path.dirname(fullLogPath)):
+        os.makedirs(os.path.dirname(fullLogPath))
+    with open(fileName,"r") as f:
+        with open(fullLogPath, "w") as f1:
+            #print fullLogPath
+            shutil.copy2(fileName,fullLogPath)
 
